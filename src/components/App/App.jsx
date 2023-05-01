54741fa195e0692ac62145ee110ae7b023564804
Viktorr22 <progdub1@gmail.com>
2023-05-01 19:42:20 +0300

    correct dz4 phonebook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,28 +2,45 @@ import { useState, useEffect } from 'react';
 import { Form, Section, ContactList, Filter } from 'components';
 import { nanoid } from 'nanoid';
 
-export function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter, setFilter] = useState('');
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-  useEffect(() => {
-    const localContacts = localStorage.getItem('Contacts');
-    const parseContacts = JSON.parse(localContacts);
-    return () => {
-      if (parseContacts) {
-        setContacts(parseContacts);
-      }
-    };
-  }, []);
+export function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('Contacts')) ?? initialContacts
+  );
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     localStorage.setItem('Contacts', JSON.stringify(contacts));
   }, [contacts]);
+
+  // export function App() {
+  //   const [contacts, setContacts] = useState([
+  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //   ]);
+  //   const [filter, setFilter] = useState('');
+
+  //   useEffect(() => {
+  //     const localContacts = localStorage.getItem('Contacts');
+  //     const parseContacts = JSON.parse(localContacts);
+  //     return () => {
+  //       if (parseContacts) {
+  //         setContacts(parseContacts);
+  //       }
+  //     };
+  //   }, []);
+
+  //   useEffect(() => {
+  //     localStorage.setItem('Contacts', JSON.stringify(contacts));
+  //   }, [contacts]);
 
   const formSubmitHandler = data => {
     const { name, number } = data;
